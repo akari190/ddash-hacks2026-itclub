b1460397dad042e491707c3c850947b4e7e6b904
Airi Kokuryo <120231703+poe125@users.noreply.github.com>
2026-02-24 13:16:51 +0900

    Delete src/supabaseClient.ts

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Supabase の設定画面からコピーした値を .env に入れている想定
-const supabaseUrl = "https://pnmqxwcokxpiexxvrmza.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubXF4d2Nva3hwaWV4eHZybXphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4ODg5OTAsImV4cCI6MjA4NzQ2NDk5MH0.RxbtkNzYLispJ8-naIchicEQXnw6rat-pU9pw9U5qcE";
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
